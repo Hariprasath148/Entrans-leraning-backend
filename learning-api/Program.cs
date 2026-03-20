e403a11467a92553c06f470262c78a95ce23ee34
hariprasathentrans <hariprasath@entrans.io>
2026-03-20 21:35:45 +0530

    for hosting

--- learning-api/Program.cs
+++ learning-api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200", "https://entrans-learning-dashboard.vercel.app")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
